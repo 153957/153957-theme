6c03cb4e97ddd06b51d8cdb553a552ce49e9fad4
Arne de Laat <arne@delaat.net>
2017-05-19 00:15:03 +0200

    Add theme templates and static as package_data.

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 from setuptools import setup, find_packages
 
-setup(name="153957-theme",
-      version="1.0.0",
-      packages=find_packages(),
-      url="http://github.com/153957/153957-theme/",
-      bugtrack_url='http://github.com/153957/153957-theme/issues',
-      license='MIT',
-      author="Arne de Laat",
-      author_email="arne@delaat.net",
-      description="Theme for sigal generated photo albums",
-      long_description=open('README.rst').read(),
-      keywords=['photo album', 'theme', 'sigal', 'galleria'],
-      classifiers=['Environment :: Plugins',
-                   'Environment :: Web Environment',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 3',
-                   'Topic :: Text Processing :: Markup :: HTML'],
-      install_requires=['sigal'],
+setup(
+    name="153957-theme",
+    version="1.0.0",
+    packages=find_packages(),
+    url="http://github.com/153957/153957-theme/",
+    bugtrack_url='http://github.com/153957/153957-theme/issues',
+    license='MIT',
+    author="Arne de Laat",
+    author_email="arne@delaat.net",
+    description="Theme for sigal generated photo albums",
+    long_description=open('README.rst').read(),
+    keywords=['photo album', 'theme', 'sigal', 'galleria'],
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    install_requires=['sigal'],
+    package_data={
+        '153957_theme': [
+            'templates/*.html',
+            'static/*/*',
+        ]
+    },
 )
