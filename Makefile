d10c2e5f4a533c42b5ca085cbde5f0059e250d4a
Arne de Laat <arne@delaat.net>
2017-05-19 00:18:26 +0200

    Add demo gallery.

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: demo
+
+demo:
+	make -C demo/ demo
---
+++ demo/sigal.conf.py
@@ -0,0 +1,31 @@
+# This configuration has been configured for this demo, not all
+# normal sigal settings have an effect in this theme.
+
+# ---------------------
+# General configuration
+# ---------------------
+
+source = 'source'
+destination = 'output'
+theme = ''
+author = '153957 Photography'
+use_orig = True
+
+# --------------------
+# Thumbnail generation
+# --------------------
+
+make_thumbs = True
+thumb_dir = 'thumbnails'
+thumb_size = (280, 140)
+thumb_fit = False
+albums_sort_attr = 'name'
+medias_sort_attr = 'date'
+ignore_directories = []
+ignore_files = []
+
+# --------
+# Plugins
+# --------
+
+plugins = ['153957_theme.full_menu', '153957_theme.theme']
